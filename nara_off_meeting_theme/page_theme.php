326315ad429d4b2f7b23cbcb40e021f16ece7c65
acliss19xx <acliss19xx@gmail.com>
2016-07-23 23:40:32 +0000

    必要項目を変更し、必要項目以外削除

--- nara_off_meeting_theme/page_theme.php
+++ nara_off_meeting_theme/page_theme.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Concrete\Theme\Elemental;
+namespace Application\Theme\NaraOffMeetingTheme;
 
 use Concrete\Core\Area\Layout\Preset\Provider\ThemeProviderInterface;
 
@@ -8,7 +8,7 @@ class PageTheme extends \Concrete\Core\Page\Theme\Theme implements ThemeProvider
 {
     public function registerAssets()
     {
-        //$this->providesAsset('javascript', 'bootstrap/*');
+/*        //$this->providesAsset('javascript', 'bootstrap/*');
         $this->providesAsset('css', 'bootstrap/*');
         $this->providesAsset('css', 'blocks/form');
         $this->providesAsset('css', 'blocks/social_links');
@@ -29,104 +29,21 @@ class PageTheme extends \Concrete\Core\Page\Theme\Theme implements ThemeProvider
         $this->requireAsset('javascript', 'picturefill');
         $this->requireAsset('javascript-conditional', 'html5-shiv');
         $this->requireAsset('javascript-conditional', 'respond');
-    }
+*/    }
 
     protected $pThemeGridFrameworkHandle = 'bootstrap3';
 
     public function getThemeName()
     {
-        return t('Elemental');
+        return t('nara off meeting theme');
     }
 
     public function getThemeDescription()
     {
-        return t('Elegant, spacious theme with support for blogs, portfolios, layouts and more.');
-    }
-
-    public function getThemeBlockClasses()
-    {
-        return array(
-            'feature' => array('feature-home-page'),
-            'page_list' => array(
-                'recent-blog-entry',
-                'blog-entry-list',
-                'page-list-with-buttons',
-                'block-sidebar-wrapped',
-            ),
-            'next_previous' => array('block-sidebar-wrapped'),
-            'share_this_page' => array('block-sidebar-wrapped'),
-            'content' => array(
-                'block-sidebar-wrapped',
-                'block-sidebar-padded',
-            ),
-            'date_navigation' => array('block-sidebar-padded'),
-            'topic_list' => array('block-sidebar-wrapped'),
-            'testimonial' => array('testimonial-bio'),
-            'image' => array(
-                'image-right-tilt',
-                'image-circle',
-            ),
-        );
-    }
-
-    public function getThemeAreaClasses()
-    {
-        return array(
-            'Page Footer' => array('area-content-accent'),
-        );
-    }
-
-    public function getThemeDefaultBlockTemplates()
-    {
-        return array(
-            'calendar' => 'bootstrap_calendar.php'
-        );
-    }
-
-    public function getThemeResponsiveImageMap()
-    {
-        return array(
-            'large' => '900px',
-            'medium' => '768px',
-            'small' => '0',
-        );
-    }
-
-    public function getThemeEditorClasses()
-    {
-        return array(
-            array('title' => t('Title Thin'), 'menuClass' => 'title-thin', 'spanClass' => 'title-thin', 'forceBlock' => 1),
-            array('title' => t('Title Caps Bold'), 'menuClass' => 'title-caps-bold', 'spanClass' => 'title-caps-bold', 'forceBlock' => 1),
-            array('title' => t('Title Caps'), 'menuClass' => 'title-caps', 'spanClass' => 'title-caps', 'forceBlock' => 1),
-            array('title' => t('Image Caption'), 'menuClass' => 'image-caption', 'spanClass' => 'image-caption', 'forceBlock' => '-1'),
-            array('title' => t('Standard Button'), 'menuClass' => '', 'spanClass' => 'btn btn-default', 'forceBlock' => '-1'),
-            array('title' => t('Success Button'), 'menuClass' => '', 'spanClass' => 'btn btn-success', 'forceBlock' => '-1'),
-            array('title' => t('Primary Button'), 'menuClass' => '', 'spanClass' => 'btn btn-primary', 'forceBlock' => '-1'),
-        );
+        return t('nara off meeting sample theme.');
     }
 
     public function getThemeAreaLayoutPresets()
     {
-        $presets = array(
-            array(
-                'handle' => 'left_sidebar',
-                'name' => 'Left Sidebar',
-                'container' => '<div class="row"></div>',
-                'columns' => array(
-                    '<div class="col-sm-4"></div>',
-                    '<div class="col-sm-8"></div>'
-                ),
-            ),
-            array(
-                'handle' => 'right_sidebar',
-                'name' => 'Right Sidebar',
-                'container' => '<div class="row"></div>',
-                'columns' => array(
-                    '<div class="col-sm-8"></div>',
-                    '<div class="col-sm-4"></div>'
-                ),
-            )
-        );
-        return $presets;
     }
 }
